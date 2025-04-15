8f9e92ff638dd9b7f30e6df436b155ae9be8afb6
Thomas Crha <thomas@9bitbyte.com>
2025-04-15 20:08:12 +1000

    added baseline for json format

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,6 +2,22 @@ local wezterm = require("wezterm") --[[@as Wezterm]] --- this type cast invokes 
 
 local M = {}
 
+-- ~/.local/state/wezterm/zombie/[WORKSPACE_NAME]/state.json
+-- Json state file structure
+-- {
+--  "version": 1,
+--  "name": "workspace_name",
+--  "tabs": [
+--    {
+--      "id": 1,
+--      "active_pane_id": 1,
+--      "panes": [
+--        {
+--          "id": 1,
+--    }
+--
+--  }
+
 M.save_workspace = function()
   print("Saving workspace...")
 end
